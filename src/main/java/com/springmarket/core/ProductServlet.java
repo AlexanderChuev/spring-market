4b5ceb6d41b50c5768f96f4bd79294ccfcbfac38
Alexander Chuev <aliaksandr.chuyeu@gmail.com>
2020-07-20 13:58:07 +0300

    web.xml servlet mapping replaced with @WebServlet

--- src/main/java/com/springmarket/core/ProductServlet.java
+++ src/main/java/com/springmarket/core/ProductServlet.java
@@ -1,16 +1,23 @@
 package com.springmarket.core;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+@WebServlet(name = "servlet1", value = {"/products", "/test"}, loadOnStartup = 0)
 public class ProductServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().print(List.of("Adidas", "Nike"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print(List.of("Reebok", "Puma"));
     }
 }
