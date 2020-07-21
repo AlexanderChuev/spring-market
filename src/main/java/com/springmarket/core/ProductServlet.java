3a70c97744dbaef97b9e51e99bb08feddc8527cf
Alexander Chuev <aliaksandr.chuyeu@gmail.com>
2020-07-21 12:32:23 +0300

    Session

--- src/main/java/com/springmarket/core/ProductServlet.java
+++ src/main/java/com/springmarket/core/ProductServlet.java
@@ -1,10 +1,13 @@
 package com.springmarket.core;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,11 +16,24 @@ public class ProductServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print(List.of("Adidas", "Nike"));
+        HttpSession session = req.getSession();
+        Cart cart = (Cart) session.getAttribute("cart");
+        resp.getWriter().print(cart);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Product product = objectMapper.readValue(req.getInputStream(), Product.class);
+        HttpSession session = req.getSession();
+        if (session.isNew()){
+            Cart cart = new Cart();
+            cart.getProducts().add(product);
+            session.setAttribute("cart", cart);
+        } else {
+            Cart cart = (Cart)session.getAttribute("cart");
+            cart.getProducts().add(product);
+        }
         resp.getWriter().print(List.of("Reebok", "Puma"));
     }
 }
